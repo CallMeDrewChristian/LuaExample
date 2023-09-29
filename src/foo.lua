eaba6ead0e612bea91c226bc2be2379388b63a08
CallMeDrewChristian <72636893+CallMeDrewChristian@users.noreply.github.com>
2023-09-29 13:41:18 -0400

    Update foo.lua

--- src/foo.lua
+++ src/foo.lua
@@ -1,1 +1,11 @@
-print("Hello World"!)
+local a = false
+function onChange()
+  if a == false then 
+    print("A is false")
+  else
+    print("A is true")
+
+  end
+end
+onChange()
+print("After Woah!")
